d7a1e78f808afeb6b7228fad6c9e542e447da76c
Quang Truong <votruong284@gmail.com>
2023-06-14 00:27:15 +0700

    Add button play again

--- examples/js/demos-basic/trucxanh.js
+++ examples/js/demos-basic/trucxanh.js
@@ -1,7 +1,7 @@
 const app = new PIXI.Application({
     width: 800,
     height: 600,
-    backgroundColor: 0x1099bb,
+    // backgroundColor: 0x1099bb,
     resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(app.view);
@@ -9,7 +9,8 @@ document.body.appendChild(app.view);
 const SCREEN_WIDTH = app.screen.width;
 const SCREEN_HEIGHT = app.screen.height;
 const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let isFirst = true
+// shuffle(data);
+let isFirst = true;
 let scoreTotal = 100;
 let score = 0;
 let demClick = 0;
@@ -34,35 +35,32 @@ const style = new PIXI.TextStyle({
     lineJoin: "round",
 });
 
+const background = PIXI.Sprite.from("examples/assets/Game_Background_142.png");
+app.stage.addChild(background);
 //SCENCE 1
 const scence1 = new PIXI.Container();
 app.stage.addChild(scence1);
 // Create score text
-const scoreTotalText = new PIXI.Text("Số điểm hiện tại = " + scoreTotal, style)
+const scoreTotalText = new PIXI.Text("Số điểm hiện tại = " + scoreTotal, style);
 scoreTotalText.x = SCREEN_WIDTH * 0.5;
 scoreTotalText.y = SCREEN_HEIGHT * 0.5 - 100;
-scoreTotalText.anchor.set(0.5)
-scence1.addChild(scoreTotalText)
+scoreTotalText.anchor.set(0.5);
+scence1.addChild(scoreTotalText);
 
 // Create container button start
 const containerButton = new PIXI.Container();
 containerButton.x = SCREEN_WIDTH * 0.5;
 containerButton.y = SCREEN_HEIGHT * 0.5;
-scence1.addChild(containerButton)
+scence1.addChild(containerButton);
 
 // Create button start
-const buttonStart = new PIXI.Graphics()
-buttonStart.lineStyle(1, 0xfeeb77, 1);
-buttonStart.beginFill(0x650a);
-buttonStart.drawRect(0, 0, 200, 50);
-buttonStart.pivot.x = buttonStart.width * 0.5;
-buttonStart.pivot.y = buttonStart.height * 0.5;
-buttonStart.endFill();
-containerButton.addChild(buttonStart)
+const buttonStart = PIXI.Sprite.from("examples/assets/blank-button-png-26.png");
+buttonStart.anchor.set(0.5);
+containerButton.addChild(buttonStart);
 
 // Create text start game
-const txtStartGame = new PIXI.Text("Start game")
-txtStartGame.anchor.set(0.5)
+const txtStartGame = new PIXI.Text("Start Game");
+txtStartGame.anchor.set(0.5);
 containerButton.addChild(txtStartGame);
 
 // Event
@@ -74,11 +72,10 @@ containerButton.on("pointerdown", () => {
     playGame();
 });
 
-
 //SCENCE 2
 const scence2 = new PIXI.Container();
 app.stage.addChild(scence2);
-scence2.visible = false
+scence2.visible = false;
 // Create text score
 const scoreText = new PIXI.Text("Score = " + score, style);
 scoreText.x = 50;
@@ -126,7 +123,6 @@ for (let i = 0; i < 20; i++) {
     container.on("pointerdown", handleChooseImg);
 }
 
-
 //SCENCE 3
 const scence3 = new PIXI.Container();
 app.stage.addChild(scence3);
@@ -135,27 +131,48 @@ scence3.visible = false;
 const victoryText = new PIXI.Text("CHÚC MỪNG BẠN ĐÃ THẮNG", style);
 victoryText.x = SCREEN_WIDTH * 0.5;
 victoryText.y = SCREEN_HEIGHT * 0.5 - 100;
-victoryText.anchor.set(0.5)
+victoryText.anchor.set(0.5);
 scence3.addChild(victoryText);
+// Create container play again
+const containerButtonPlayAgain = new PIXI.Container();
+containerButtonPlayAgain.x = SCREEN_WIDTH * 0.5;
+containerButtonPlayAgain.y = SCREEN_HEIGHT * 0.5;
+scence3.addChild(containerButtonPlayAgain);
 
+// Create button play again
+const buttonPlayAgain = PIXI.Sprite.from(
+    "examples/assets/blank-button-png-26.png"
+);
+buttonPlayAgain.anchor.set(0.5);
+containerButtonPlayAgain.addChild(buttonPlayAgain);
+
+// Create text play again
+const txtPlayAgain = new PIXI.Text("Chơi lại");
+txtPlayAgain.anchor.set(0.5);
+containerButtonPlayAgain.addChild(txtPlayAgain);
+
+containerButtonPlayAgain.interactive = true;
+containerButtonPlayAgain.cursor = "pointer";
+containerButtonPlayAgain.on("pointerdown", () => {
+    scence3.visible = false;
+    scence2.visible = true;
+    playGame();
+});
+
+// Create container back
 const containerButtonBack = new PIXI.Container();
 containerButtonBack.x = SCREEN_WIDTH * 0.5;
-containerButtonBack.y = SCREEN_HEIGHT * 0.5;
-scence3.addChild(containerButtonBack)
+containerButtonBack.y = SCREEN_HEIGHT * 0.5 + 80;
+scence3.addChild(containerButtonBack);
 
 // Create button back
-const buttonBack = new PIXI.Graphics()
-buttonBack.lineStyle(1, 0xfeeb77, 1);
-buttonBack.beginFill(0x650a);
-buttonBack.drawRect(0, 0, 200, 50);
-buttonBack.pivot.x = buttonBack.width * 0.5;
-buttonBack.pivot.y = buttonBack.height * 0.5;
-buttonBack.endFill();
-containerButtonBack.addChild(buttonBack)
+const buttonBack = PIXI.Sprite.from("examples/assets/blank-button-png-26.png");
+buttonBack.anchor.set(0.5);
+containerButtonBack.addChild(buttonBack);
 
 // Create text back game
-const txtBackGame = new PIXI.Text("Quay lại")
-txtBackGame.anchor.set(0.5)
+const txtBackGame = new PIXI.Text("Quay lại");
+txtBackGame.anchor.set(0.5);
 containerButtonBack.addChild(txtBackGame);
 
 containerButtonBack.interactive = true;
@@ -166,31 +183,27 @@ containerButtonBack.on("pointerdown", () => {
 });
 
 function playGame() {
-    if(!isFirst) {
+    if (!isFirst) {
         score = 0;
-        shuffle(data)
-        //Dau tien kiem tra da tao chua, neu chua thi tao, con tao roi thi for de bat
-
+        shuffle(data);
         const containerList = scence2.children;
-        const text = containerList[0]
-        containerList.shift()
+        const text = containerList[0];
+        text.text = "Score = " + score;
+        containerList.shift();
         for (let i = 0; i < 20; i++) {
-            containerList[i].visible = true
-            containerList[i].children[1].visible = true
-            containerList[i].children[2].visible = true
-
-            // const imgNew = PIXI.Sprite.from("examples/assets/" + data[i] + ".jpg");
-            // imgNew.x = 100 * 0.5;
-            // imgNew.y = 100 * 0.5;
-            // imgNew.anchor.set(0.5);
-            // imgNew.value = data[i];
-
-            let textureTmp = PIXI.Texture.from("examples/assets/" + data[i] + ".jpg");
+            // [Sprite, _Graphics, _Text]
+            containerList[i].visible = true;
+            containerList[i].children[1].visible = true;
+            containerList[i].children[2].visible = true;
+            let textureTmp = PIXI.Texture.from(
+                "examples/assets/" + data[i] + ".jpg"
+            );
             containerList[i].children[0].texture = textureTmp;
+            containerList[i].children[0].value = data[i];
         }
         containerList.unshift(text);
     }
-    isFirst = false
+    isFirst = false;
 }
 
 async function handleChooseImg(event) {
@@ -217,8 +230,9 @@ async function handleChooseImg(event) {
                     containerPrevious.visible = false;
                     scoreText.text = "Score = " + ++score;
                     if (score == 10) {
-                        scoreTotal+=score
-                        scoreTotalText.text = "Số điểm hiện tại = " + scoreTotal
+                        scoreTotal += score;
+                        scoreTotalText.text =
+                            "Số điểm hiện tại = " + scoreTotal;
                         scence2.visible = false;
                         scence3.visible = true;
                     }
